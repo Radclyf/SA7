1ee90b6e4420c91a39fa1a2f023afd59464bf1f4
Radclyf <shanksakagame66@gmail.com>
2023-01-15 01:00:53 +0800

    MILESTONE C: Create Figure class and subclasses MILESTONE D: Create and show fields for each figure MILESTONE E: Error checking for inputs

--- src/Ellipsoid.java
+++ src/Ellipsoid.java
@@ -1,20 +1,37 @@
+import javax.swing.*;
+
 public class Ellipsoid extends Figure{
-    private double r1;
-    private double r2;
-    private double r3;
-    public Ellipsoid(double r1, double r2, double r3) {
-        this.r1 = r1;
-        this.r2 = r2;
-        this.r3 = r3;
+    private double a;
+    private double b;
+    private double c;
+
+    public Ellipsoid(double a, double b, double c) {
+        try {
+            if(a < 0) {
+                throw new IllegalArgumentException("A-axis cannot be negative");
+            }
+            this.a = a;
+            if(b < 0) {
+                throw new IllegalArgumentException("B-axis cannot be negative");
+            }
+            this.b = b;
+            if(c < 0) {
+                throw new IllegalArgumentException("C-axis cannot be negative");
+            }
+            this.c = c;
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Please enter a valid number");
+        }
+
     }
     @Override
     public double getSurfaceArea() {
-        return 4 * Math.PI * Math.sqrt((r1*r2 + r2*r3 + r1*r3)/3);
+        return 4 * Math.PI * Math.sqrt(((a*b) + (b*c) + (a*c))/3);
     }
 
     @Override
     public double getVolume() {
-        return (4 / 3) * Math.PI * r1 * r2 * r3;
+        return (4/3) * Math.PI * a * b * c;
     }
 
     @Override
@@ -26,7 +43,15 @@ public class Ellipsoid extends Figure{
         private double radius2;
         public Sphere(double radius2) {
             super(radius2, radius2, radius2);
-            this.radius2 = radius2;
+            try {
+                if(radius2 < 0) {
+                    throw new IllegalArgumentException("Radius cannot be negative");
+                }
+                this.radius2 = radius2;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid number for radius");
+            }
+
         }
 
         @Override
