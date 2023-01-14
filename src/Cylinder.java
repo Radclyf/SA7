45edbefb43f87f12b709c187c2438f194ea7bd22
Radclyf <shanksakagame66@gmail.com>
2023-01-14 20:07:55 +0800

    MILESTONE C: Create Figure class and subclasses MILESTONE D: Create and show fields for each figure

---
+++ src/Cylinder.java
@@ -0,0 +1,18 @@
+public class Cylinder extends Figure{
+    private double radius;
+    private double height2;
+
+    public Cylinder(double radius, double height2) {
+        this.height2 = height2;
+        this.radius = radius;
+    }
+    @Override
+    public double getSurfaceArea() {
+        return 2 * Math.PI * radius * height2 + 2 * Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.PI * Math.pow(radius, 2) * height2;
+    }
+}
