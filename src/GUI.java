4d25357d1742bde5562bed02630bbe5737541511
Radclyf <shanksakagame66@gmail.com>
2023-01-15 14:09:49 +0800

    MILESTONE A: Show the GUI

--- src/GUI.java
+++ src/GUI.java
@@ -4,4 +4,13 @@ public class GUI {
     private JPanel mainPanel;
     private JComboBox comboBox1;
     private JButton saveButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("GUI");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setContentPane(new GUI().mainPanel);
+        frame.setVisible(true);
+    }
 }
