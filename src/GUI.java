fcac8320945aaa5dddbcb917109dc46463a1b39f
Radclyf <shanksakagame66@gmail.com>
2023-01-15 19:14:55 +0800

    MILESTONE D: Create and show fields for each figure MILESTONE E: Error checking for inputs

--- src/GUI.java
+++ src/GUI.java
@@ -25,6 +25,7 @@ public class GUI {
     private JButton volumeSortButton;
     private JButton surfaceAreaSortButton;
     private JButton nameSortButton;
+    private JTextField sideField;
 
 
     public GUI() {
@@ -38,76 +39,156 @@ public class GUI {
 
                 switch (shape) {
                     case "Cylinder":
-                        double radius = Double.parseDouble(radiusField.getText());
-                        double height2 = Double.parseDouble(heightField.getText());
-                        Cylinder Cylinder = new Cylinder(radius, height2);
-                        surfaceArea = Cylinder.getSurfaceArea();
-                        volume = Cylinder.getVolume();
+                        try {
+                            double radius = Double.parseDouble(radiusField.getText());
+                            double height2 = Double.parseDouble(heightField.getText());
+                            if(radius < 0 || height2 < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: radius and height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                Cylinder Cylinder = new Cylinder(radius, height2);
+                                surfaceArea = Cylinder.getSurfaceArea();
+                                volume = Cylinder.getVolume();
+                            }
+                        } catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: radius and height must be numerical values.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Ellipsoid":
-                        double a = Double.parseDouble(lengthField.getText());
-                        double b = Double.parseDouble(widthField.getText());
-                        double c = Double.parseDouble(heightField.getText());
-                        Ellipsoid Ellipsoid = new Ellipsoid(a, b, c);
-                        surfaceArea = Ellipsoid.getSurfaceArea();
-                        volume = Ellipsoid.getVolume();
+                        try {
+                            double a = Double.parseDouble(lengthField.getText());
+                            double b = Double.parseDouble(widthField.getText());
+                            double c = Double.parseDouble(heightField.getText());
+                            if(a < 0 || b < 0 || c < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: A-axis, B-axis and C-axis must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                Ellipsoid Ellipsoid = new Ellipsoid(a, b, c);
+                                surfaceArea = Ellipsoid.getSurfaceArea();
+                                volume = Ellipsoid.getVolume();
+                            }
+                        } catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: A-axis, B-axis and C-axis must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Sphere":
-                        double radius2 = Double.parseDouble(radiusField.getText());
-                        Ellipsoid Sphere = new Ellipsoid.Sphere(radius2);
-                        surfaceArea = Sphere.getSurfaceArea();
-                        volume = Sphere.getVolume();
+                        try {
+                            double radius2 = Double.parseDouble(radiusField.getText());
+                            if(radius2 < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Radius must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                Ellipsoid Sphere = new Ellipsoid.Sphere(radius2);
+                                surfaceArea = Sphere.getSurfaceArea();
+                                volume = Sphere.getVolume();
+                            }
+                        } catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Radius must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Prism":
-                        double base = Double.parseDouble(baseField.getText());
-                        double perimeter = Double.parseDouble(perimeterField.getText());
-                        double height3 = Double.parseDouble(heightField.getText());
-                        Prism Prism = new Prism(base, perimeter, height3);
-                        surfaceArea = Prism.getSurfaceArea();
-                        volume = Prism.getVolume();
+                        try {
+                            double base = Double.parseDouble(baseField.getText());
+                            double perimeter = Double.parseDouble(perimeterField.getText());
+                            double height3 = Double.parseDouble(heightField.getText());
+                            if(base < 0 || perimeter < 0 || height3 < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Base, Perimeter and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                Prism Prism = new Prism(base, perimeter, height3);
+                                surfaceArea = Prism.getSurfaceArea();
+                                volume = Prism.getVolume();
+                            }
+                        } catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Base, Perimeter and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Rectangular Pyramid":
-                        double length1 = Double.parseDouble(lengthField.getText());
-                        double width1 = Double.parseDouble(widthField.getText());
-                        double height1 = Double.parseDouble(heightField.getText());
-                        RectangularPyramid RectangularPyramid = new RectangularPyramid(length1, width1, height1);
-                        surfaceArea = RectangularPyramid.getSurfaceArea();
-                        volume = RectangularPyramid.getVolume();
+                        try {
+                            double length1 = Double.parseDouble(lengthField.getText());
+                            double width1 = Double.parseDouble(widthField.getText());
+                            double height1 = Double.parseDouble(heightField.getText());
+                            if (length1 < 0 || width1 < 0 || height1 < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Length, Width and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                RectangularPyramid RectangularPyramid = new RectangularPyramid(length1, width1, height1);
+                                surfaceArea = RectangularPyramid.getSurfaceArea();
+                                volume = RectangularPyramid.getVolume();
+                            }
+                        }catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Length, Width and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Pyramid":
-                        double baseArea = Double.parseDouble(lengthField.getText());
-                        double perimeter1 = Double.parseDouble(widthField.getText());
-                        double height4 = Double.parseDouble(heightField.getText());
-                        RectangularPyramid Pyramid = new RectangularPyramid.Pyramid(baseArea, perimeter1, height4);
-                        surfaceArea = Pyramid.getSurfaceArea();
-                        volume = Pyramid.getVolume();
+                        try {
+                            double baseArea = Double.parseDouble(lengthField.getText());
+                            double perimeter1 = Double.parseDouble(widthField.getText());
+                            double height4 = Double.parseDouble(heightField.getText());
+                            if (baseArea < 0 || perimeter1 < 0 || height4 < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Base, Perimeter and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                RectangularPyramid Pyramid = new RectangularPyramid.Pyramid(baseArea, perimeter1, height4);
+                                surfaceArea = Pyramid.getSurfaceArea();
+                                volume = Pyramid.getVolume();
+                            }
+                        }catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Base, Perimeter and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Rectangular Solid":
-                        double length = Double.parseDouble(lengthField.getText());
-                        double width = Double.parseDouble(widthField.getText());
-                        double height = Double.parseDouble(heightField.getText());
-                        RectangularSolid RectangularSolid = new RectangularSolid(length, width, height);
-                        surfaceArea = RectangularSolid.getSurfaceArea();
-                        volume = RectangularSolid.getVolume();
+                        try {
+                            double length = Double.parseDouble(lengthField.getText());
+                            double width = Double.parseDouble(widthField.getText());
+                            double height = Double.parseDouble(heightField.getText());
+                            if (length < 0 || width < 0 || height < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Length, Width and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                RectangularSolid RectangularSolid = new RectangularSolid(length, width, height);
+                                surfaceArea = RectangularSolid.getSurfaceArea();
+                                volume = RectangularSolid.getVolume();
+                            }
+                        }catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Length, Width and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Cube":
-                        double side = Double.parseDouble(lengthField.getText());
-                        RectangularSolid Cube = new RectangularSolid.Cube(side);
-                        surfaceArea = Cube.getSurfaceArea();
-                        volume = Cube.getVolume();
+                        try {
+                            double side = Double.parseDouble(sideField.getText());
+                            if (side < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Side must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                RectangularSolid Cube = new RectangularSolid.Cube(side);
+                                surfaceArea = Cube.getSurfaceArea();
+                                volume = Cube.getVolume();
+                            }
+                        }catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Side must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Right Circular Cone":
-                        double radius1 = Double.parseDouble(radiusField.getText());
-                        double height5 = Double.parseDouble(heightField.getText());
-                        RightCircularCone RightCircularCone = new RightCircularCone(radius1, height5);
-                        surfaceArea = RightCircularCone.getSurfaceArea();
-                        volume = RightCircularCone.getVolume();
+                        try {
+                            double radius1 = Double.parseDouble(radiusField.getText());
+                            double height5 = Double.parseDouble(heightField.getText());
+                            if (radius1 < 0 || height5 < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Radius and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                RightCircularCone RightCircularCone = new RightCircularCone(radius1, height5);
+                                surfaceArea = RightCircularCone.getSurfaceArea();
+                                volume = RightCircularCone.getVolume();
+                            }
+                        }catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Radius and Height must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                     case "Tetrahedron":
-                        double sideLength = Double.parseDouble(lengthField.getText());
-                        Tetrahedron Tetrahedron = new Tetrahedron(sideLength);
-                        surfaceArea = Tetrahedron.getSurfaceArea();
-                        volume = Tetrahedron.getVolume();
+                        try {
+                            double sideLength = Double.parseDouble(sideField.getText());
+                            if (sideLength < 0) {
+                                JOptionPane.showMessageDialog(null, "Error: Side must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                Tetrahedron Tetrahedron = new Tetrahedron(sideLength);
+                                surfaceArea = Tetrahedron.getSurfaceArea();
+                                volume = Tetrahedron.getVolume();
+                            }
+                        }catch (NumberFormatException exception) {
+                            JOptionPane.showMessageDialog(null, "Error: Side must be positive numbers.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        }
                         break;
                 }
 
@@ -120,6 +201,7 @@ public class GUI {
                 baseField.setEnabled(false);
                 perimeterField.setEnabled(false);
                 radiusField.setEnabled(false);
+                sideField.setEnabled(false);
 
 
                 DecimalFormat df = new DecimalFormat("#.##");
@@ -137,6 +219,7 @@ public class GUI {
                 baseField.setText("");
                 perimeterField.setText("");
                 radiusField.setText("");
+                sideField.setText("");
                 comboBox1.setSelectedIndex(0);
                 nameField.setEnabled(true);
                 comboBox1.setEnabled(true);
@@ -147,7 +230,7 @@ public class GUI {
                 baseField.setEnabled(true);
                 perimeterField.setEnabled(true);
                 radiusField.setEnabled(true);
-
+                sideField.setEnabled(true);
 
             }
         });
